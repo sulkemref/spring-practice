93e34ffb0ad0cdd3c54c1a55f3de49e9e72f1971
Kemail-terminal <sulkemref@gmail.com>
2023-02-22 18:11:47 -0600

    spring-03-Wiring

--- spring-03-Wiring/src/main/java/com/cydeo/casefactory/Case.java
+++ spring-03-Wiring/src/main/java/com/cydeo/casefactory/Case.java
@@ -10,12 +10,20 @@ public abstract class Case {
     private String model;
     private String manufacturer;
     private String powerSupply;
+    private Dimensions dimensions;
 
-    public Case(String model, String manufacturer, String powerSupply) {
+    public Case(String model, String manufacturer, String powerSupply,Dimensions dimensions) {
         this.model = model;
         this.manufacturer = manufacturer;
         this.powerSupply = powerSupply;
+        this.dimensions = dimensions;
     }
+
+//    public Case(String model, String manufacturer, String powerSupply) {
+//        this.model = model;
+//        this.manufacturer = manufacturer;
+//        this.powerSupply = powerSupply;
+//    }
 
     public abstract void pressPowerButton();
 }
