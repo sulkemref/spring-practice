63f0bbc1796b8da82e4947ee2e96f541c7f1186b
Kemail-terminal <sulkemref@gmail.com>
2023-03-04 16:31:14 -0600

    spring-09-MVC-Model

---
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,34 @@
+package com.cydeo.controller;
+
+import com.cydeo.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("/register")
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+    }
+
+    @RequestMapping("/drop")
+    public String drop(){
+
+        return "student/register";
+    }
+
+   @RequestMapping("/welcome")
+    public String welcome(@RequestParam int id){
+       System.out.println(id);
+
+        return "student/welcome";
+    }
+
+}
