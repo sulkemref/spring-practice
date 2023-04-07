3bb2a21ed948ca55505a1112a45a229b4ef74ce0
Kemail-terminal <sulkemref@gmail.com>
2023-04-06 20:37:25 -0500

    spring-11-Data Generator

---
+++ spring-11-Data-TableCreation/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE employees(
+
+                          id bigint not null,
+                          name varchar(255),
+                          primary key (id)
+
+);
