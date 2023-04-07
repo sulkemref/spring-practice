9f0a20eaac98686b9ea71c0d09236f7cca12f806
Kemail-terminal <sulkemref@gmail.com>
2023-04-07 11:06:17 -0500

    spring-11-Data TableCreation Done

--- spring-11-Data-TableCreation/src/main/resources/schema.sql
+++ spring-11-Data-TableCreation/src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS employees;
-
-CREATE TABLE employees(
-
-                          id bigint not null,
-                          name varchar(255),
-                          primary key (id)
-
-);
+-- DROP TABLE IF EXISTS employees;
+--
+-- CREATE TABLE employees(
+--
+--                           id bigint not null,
+--                           name varchar(255),
+--                           primary key (id)
+--
+-- );
