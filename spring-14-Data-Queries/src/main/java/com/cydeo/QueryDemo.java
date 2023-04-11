9a2ad2c8b6d192549d97d307a4e959f8b15e4465
Kemail-terminal <sulkemref@gmail.com>
2023-04-11 17:45:06 -0500

    spring-14 Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+import com.cydeo.repository.RegionRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryDemo implements CommandLineRunner {
+
+    private final RegionRepository regionRepository;
+
+    public QueryDemo(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("findByCountry: "+ regionRepository.findByCountry("Canada"));
+        System.out.println("-------------");
+        System.out.println("findByCountryContaining: "+regionRepository.findByCountryContaining("United"));
+        System.out.println("-------------");
+        System.out.println("findByCountryContainingOrderByRegionDesc: "+regionRepository.findByCountryContainingOrderByRegionDesc("United"));
+        System.out.println("-------------");
+        System.out.println("findTop2ByCountry: "+regionRepository.findTop2ByCountry("United States"));
+    }
+}
