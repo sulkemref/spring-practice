f114a29542e73ce163c153fb9d513fc0c23e6ae0
Kemail-terminal <sulkemref@gmail.com>
2023-06-16 11:54:17 -0500

    spring-21 AOP @Around

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.cydeo.aspect;
 
 import com.cydeo.dto.CourseDTO;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -105,5 +106,30 @@ public class LoggingAspect {
 //                exception.getMessage());
 //    }
 
+    @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
+    public void loggingAnnotationPC(){}
+
+    @Around("loggingAnnotationPC()")
+    public Object anyLoggingAnnotationOperation(ProceedingJoinPoint proceedingJointPoint){
+
+        logger.info("Before -> Method: {} - Parameter {}",
+                proceedingJointPoint.getSignature().toShortString(),
+                proceedingJointPoint.getArgs());
+
+        Object result = null;
+
+        try{
+            result = proceedingJointPoint.proceed();
+        } catch (Throwable throwable){
+            throwable.printStackTrace();
+        }
+
+        logger.info("After -> Method: {} - Result: {}",
+                proceedingJointPoint.getSignature().toShortString(),
+                result.toString());
+
+        return result;
+
+    }
 
 }
